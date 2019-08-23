ddf6786f56ba3a001cfd2236ef6c032998732bc0
hsrsbd <hsraffety@gmail.com>
2019-08-23 15:39:55 -0700

    package.json updated with proxy and prettier script for consistency. implemented partial register.

--- src/components/register.js
+++ src/components/register.js
@@ -1,26 +1,25 @@
-import React, {useState} from 'react'
-
+import React, { useState } from "react"
 
 const Register = () => {
-    const [user, setUser] = useState({
-            first_name: '', 
-            last_name: '', 
-            track_id: 1, 
-            email: '', 
-            password: ''
-        })
-    
-    return (
-        <form>
-            <input
-                name='first_name'
-                value={user.first_name}
-                type={Text}
-                onChange={userHandler}
-                placeholder='First Name'
-            />
-        </form>
-    )
+  const [user, setUser] = useState({
+    first_name: "",
+    last_name: "",
+    track_id: 1,
+    email: "",
+    password: ""
+  })
+
+  return (
+    <form>
+      <input
+        name="first_name"
+        value={user.first_name}
+        type={Text}
+        onChange={userHandler}
+        placeholder="First Name"
+      />
+    </form>
+  )
 }
 
 export default Register
