53f3059172e81842e1f8cf67ace5c0fb173f9a66
Adam Mathieson <mathieson1222@gmail.com>
2019-08-25 15:44:14 -0700

    bug fix on email input now updates newUser.email

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -37,7 +37,7 @@ const SignUp = () => {
             />
              <input
                 name='email'
-                value={newUser.first_name}
+                value={newUser.email}
                 type='text'
                 onChange={newUserHandler}
                 placeholder='Email'
