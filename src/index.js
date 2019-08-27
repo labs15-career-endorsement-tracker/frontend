ebdf5fd7b9e4304d49ae62e22385fe8181c02150
Adam Mathieson <mathieson1222@gmail.com>
2019-08-27 14:00:40 -0700

    adds redux, thunk, logger dependencies - store setup in index.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,24 @@ import ReactDOM from "react-dom"
 import "./index.css"
 import App from "./App"
 import { BrowserRouter as Router } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+import logger from 'redux-logger'
+
+const store = createStore(
+    rootReducer,
+    compose(
+        applyMiddleware(thunk, logger),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+)
 
 ReactDOM.render(
-    <Router>
-        <App />
-    </Router>
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>
     ,
     document.getElementById("root"))
