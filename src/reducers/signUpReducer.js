15b3e4eff1e714d11a05115a4d0c7c190f31a490
Adam Mathieson <mathieson1222@gmail.com>
2019-08-27 15:27:20 -0700

    adds signUpReducer. initialState and switch

---
+++ src/reducers/signUpReducer.js
@@ -0,0 +1,29 @@
+import { SIGNUP_IN_PROGRESS, SIGNUP_SUCCESS, SIGNUP_FAILURE } from '../actions'
+
+const initialState = {
+    inProgress: false,
+    serverError: ''
+}
+
+export const signUpReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case SIGNUP_IN_PROGRESS:
+            return {
+                ...state,
+                inProgress: true
+            }
+            case SIGNUP_SUCCESS:
+                return {
+                    ...state,
+                    inProgress: false
+                }
+            case SIGNUP_FAILURE:
+                return {
+                    ...state,
+                    inProgress: false,
+                    serverError: action.payload
+                }
+            default:
+                return state
+    }
+}
