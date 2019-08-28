f3e373d452ba032ebd62524cadbd724441a6745f
hsrsbd <hsraffety@gmail.com>
2019-08-28 08:21:14 -0700

    Implements node-sass for styling. Restructures files (app, index)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Route } from "react-router-dom"
 
-import "./App.css"
+import "./App.scss"
 
 import SignUp from "./components/SignUp"
 import Login from "./components/Login"
