e7a8fc7635d6535498da1ce09bf2609fe8251baf
Adam Mathieson <mathieson1222@gmail.com>
2019-08-28 13:10:50 -0700

    format

--- src/actions/signUpAction.js
+++ src/actions/signUpAction.js
@@ -1,27 +1,27 @@
-import axios from 'axios'
+import axios from "axios"
 
-export const SIGNUP_IN_PROGRESS = 'SIGNUP_IN_PROGRESS'
-export const SIGNUP_SUCCESS = 'SIGNUP_SUCCESS'
-export const SIGNUP_FAILURE = 'SIGNUP_FAILURE'
+export const SIGNUP_IN_PROGRESS = "SIGNUP_IN_PROGRESS"
+export const SIGNUP_SUCCESS = "SIGNUP_SUCCESS"
+export const SIGNUP_FAILURE = "SIGNUP_FAILURE"
 
 export const createUser = newUser => dispatch => {
-    dispatch({
-        type: SIGNUP_IN_PROGRESS
+  dispatch({
+    type: SIGNUP_IN_PROGRESS
+  })
+  return axios
+    .post("http://localhost:5000/api/v0/users", newUser)
+    .then(res => {
+      console.log(res)
+      localStorage.setItem("token", res.data.token)
+      localStorage.setItem("userId", res.data.userId)
+      dispatch({
+        type: SIGNUP_SUCCESS
+      })
     })
-    return axios
-        .post('http://localhost:5000/api/v0/users', newUser)
-        .then(res => {
-            console.log(res)
-            localStorage.setItem('token', res.data.token)
-            localStorage.setItem('userId', res.data.userId)
-            dispatch({
-                type: SIGNUP_SUCCESS
-            })
-    })
-    .catch(err => 
-                dispatch({
-                    type: SIGNUP_FAILURE,
-                    payload: err
-            }) 
-        )
+    .catch(err =>
+      dispatch({
+        type: SIGNUP_FAILURE,
+        payload: err
+      })
+    )
 }
