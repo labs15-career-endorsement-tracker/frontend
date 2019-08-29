6b0c7457daceff3f361a412f3fc61268fb30a2c8
hsrsbd <hsraffety@gmail.com>
2019-08-29 16:14:37 -0700

    Updating File Scaffolding for Components/Styles

---
+++ src/components/Dropdown/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import "../styles/index"
+
+const Dropdown = props => {
+  return (
+    <div className="dropdown-container">
+      <span></span>
+    </div>
+  )
+}
+
+export default Dropdown
