8342884bd92c1b2c095b759aed4a1080cabca5da
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-08-28 20:13:45 -0400

    Creates a 'Dashboard' component and creates a Route for it in App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import "./styles/index.scss"
 import SignUp from "./components/SignUp"
 import Login from "./components/Login"
 import Navigation from "./components/Navigation"
+import Dashboard from "./components/Dashboard"
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
       <Navigation />
       <Route exact path="/" component={Login} />
       <Route path="/sign-up" component={SignUp} />
+      <Route path="/dashboard" component={Dashboard} />
     </div>
   )
 }
