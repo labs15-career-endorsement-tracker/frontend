8585917bd993d183be9e7a1f396a5f928d39c3b9
Adam Mathieson <mathieson1222@gmail.com>
2019-08-29 23:44:49 -0700

    RequirementCardContainer rendered in dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,12 @@
 import React from "react"
 import UserInfo from "./UserInfo"
+import RequirementCardContainer from "./requirement/RequirementCardContainer";
 
 const Dashboard = () => {
   return (
     <div className="dash-container">
       <UserInfo />
+      <RequirementCardContainer />
     </div>
   )
 }
---
+++ src/components/requirement/RequirementCardContainer.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const RequirementCardContainer = () => {
+  return (
+    <div className="requirement-card-container">
+        <h1>Requirements</h1>
+
+    </div>
+  )
+}
+
+export default RequirementCardContainer
