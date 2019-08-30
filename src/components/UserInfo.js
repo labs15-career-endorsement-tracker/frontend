f7bee89891f5a2a05bddb5e702e46ad2b89fdc44
Adam Mathieson <mathieson1222@gmail.com>
2019-08-29 19:32:15 -0700

    UserInfo now sets users first_name and renders in message

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,11 +1,18 @@
 import React, {useState, useEffect} from 'react'
+import axios from 'axios';
 
 const UserInfo = () => {
     const [user, setUser] = useState({first_name: ''})
 
     useEffect(() => {
         const userId = localStorage.getItem('userId')
-    })
+        axios
+            .get('http://localhost:5000/api/v0/users')
+            .then(res => {
+                const filtUser = res.data.filter(user => user.id === parseInt(userId))
+                setUser(filtUser[0])
+            })
+    }, [])
 
     return (
         <div className='userInfo-container'>
