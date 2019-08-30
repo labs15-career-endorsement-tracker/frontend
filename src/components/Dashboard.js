1634165b7aadfefbb1ea2303424f231d415c117b
Adam Mathieson <mathieson1222@gmail.com>
2019-08-30 16:05:55 -0700

    StepList boilerplate rendered in Dashboar for developement

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,14 @@
 import React from "react"
 import UserInfo from "./UserInfo"
 import RequirementCardContainer from "./requirement/RequirementCardContainer"
+import StepList from "./steps/StepList"
 
 const Dashboard = () => {
   return (
     <div className="dash-container">
       <UserInfo />
       <RequirementCardContainer />
+      <StepList />
     </div>
   )
 }
---
+++ src/components/steps/StepList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const StepList = () => {
+    return (
+        <div>
+            <h1>Steps to complete</h1>
+        </div>
+    );
+}
+
+export default StepList;
