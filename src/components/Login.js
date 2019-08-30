5d8f2b91c7141da842ea2f5e59fa749776d736d1
hsrsbd <hsraffety@gmail.com>
2019-08-30 10:17:06 -0700

    Updates background img source.

--- src/components/Login.js
+++ src/components/Login.js
@@ -19,106 +19,108 @@ const Login = props => {
   }
 
   return (
-    <div className="login-container">
-      <div className="warning-container">
-        {error.errorCode && error.errorMessage ? (
-          <div className="warned">
-            <h2>Status {error.errorCode}</h2>
-            <h6>{error.errorMessage}. Please try again.</h6>
-          </div>
-        ) : null}
+    <div className="wrapper">
+      <div className="login-container">
+        <div className="warning-container">
+          {error.errorCode && error.errorMessage ? (
+            <div className="warned">
+              <h2>Status {error.errorCode}</h2>
+              <h6>{error.errorMessage}. Please try again.</h6>
+            </div>
+          ) : null}
+        </div>
+        <div className="brand">
+          <h1>ENDRSD</h1>
+        </div>
+        <Formik
+          initialValues={{ email: "", password: "" }}
+          onSubmit={(values, { setSubmitting, resetForm }) => {
+            setSubmitting(true)
+            axios
+              .post("/api/v0/login", values)
+              .then(res => {
+                localStorage.setItem("token", res.data.token)
+                resetForm()
+                props.history.push("/profile")
+              })
+              .catch(err => {
+                setSubmitting(false)
+                handleError(err)
+                resetForm()
+              })
+          }}
+          validationSchema={Yup.object().shape({
+            email: Yup.string()
+              .email()
+              .required("Please enter a valid email address"),
+            password: Yup.string()
+              .min(8, "Your password must be more than 8 characters")
+              .max(16, "Your password must be less than than 16 characters")
+              .required("Please enter your password")
+          })}
+        >
+          {props => {
+            const {
+              values,
+              touched,
+              errors,
+              isSubmitting,
+              handleChange,
+              handleBlur,
+              handleSubmit
+            } = props
+            return (
+              <form onSubmit={handleSubmit}>
+                <label htmlFor="email" style={{ display: "block" }}>
+                  Email Address
+                </label>
+                <input
+                  id="email"
+                  placeholder="you@email.com"
+                  type="text"
+                  value={values.email}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={
+                    errors.email && touched.email
+                      ? "text-input error"
+                      : "text-input"
+                  }
+                />
+                <div className="error-container">
+                  {errors.email && touched.email && (
+                    <div className="input-feedback">{errors.email}</div>
+                  )}
+                </div>
+                <label htmlFor="password" style={{ display: "block" }}>
+                  Password
+                </label>
+                <input
+                  id="password"
+                  placeholder="password"
+                  type="password"
+                  value={values.password}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={
+                    errors.password && touched.password
+                      ? "text-input error"
+                      : "text-input"
+                  }
+                />
+                <div className="error-container">
+                  {errors.password && touched.password && (
+                    <div className="input-feedback">{errors.password}</div>
+                  )}
+                </div>
+                <button type="submit" disabled={isSubmitting}>
+                  Sign In
+                </button>
+              </form>
+            )
+          }}
+        </Formik>
       </div>
-      <div className="brand">
-        <h1>ENDRSD</h1>
-      </div>
-      <Formik
-        initialValues={{ email: "", password: "" }}
-        onSubmit={(values, { setSubmitting, resetForm }) => {
-          setSubmitting(true)
-          axios
-            .post("/api/v0/login", values)
-            .then(res => {
-              localStorage.setItem("token", res.data.token)
-              resetForm()
-              props.history.push("/profile")
-            })
-            .catch(err => {
-              setSubmitting(false)
-              handleError(err)
-              resetForm()
-            })
-        }}
-        validationSchema={Yup.object().shape({
-          email: Yup.string()
-            .email()
-            .required("Please enter a valid email address"),
-          password: Yup.string()
-            .min(8, "Your password must be more than 8 characters")
-            .max(16, "Your password must be less than than 16 characters")
-            .required("Please enter your password")
-        })}
-      >
-        {props => {
-          const {
-            values,
-            touched,
-            errors,
-            isSubmitting,
-            handleChange,
-            handleBlur,
-            handleSubmit
-          } = props
-          return (
-            <form onSubmit={handleSubmit}>
-              <label htmlFor="email" style={{ display: "block" }}>
-                Email Address
-              </label>
-              <input
-                id="email"
-                placeholder="you@email.com"
-                type="text"
-                value={values.email}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                className={
-                  errors.email && touched.email
-                    ? "text-input error"
-                    : "text-input"
-                }
-              />
-              <div className="error-container">
-                {errors.email && touched.email && (
-                  <div className="input-feedback">{errors.email}</div>
-                )}
-              </div>
-              <label htmlFor="password" style={{ display: "block" }}>
-                Password
-              </label>
-              <input
-                id="password"
-                placeholder="password"
-                type="password"
-                value={values.password}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                className={
-                  errors.password && touched.password
-                    ? "text-input error"
-                    : "text-input"
-                }
-              />
-              <div className="error-container">
-                {errors.password && touched.password && (
-                  <div className="input-feedback">{errors.password}</div>
-                )}
-              </div>
-              <button type="submit" disabled={isSubmitting}>
-                Sign In
-              </button>
-            </form>
-          )
-        }}
-      </Formik>
     </div>
   )
 }
