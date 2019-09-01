5bf9bf19b5bf46d5ea8211f16b0639575bad117f
Adam Mathieson <mathieson1222@gmail.com>
2019-09-01 16:02:27 -0700

    tasks added to state variable in StepList from reducer

--- src/components/steps/StepList.js
+++ src/components/steps/StepList.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux'
 
-const StepList = () => {
+
+const StepList = props => {
+    const [steps, setSteps] = useState([])
+    console.log(steps)
+
+    useEffect(() => {
+        setSteps(props.stepsByTask)
+    }, [props.stepsByTask])
     return (
         <div>
             <h1>Steps to complete</h1>
@@ -8,4 +16,13 @@ const StepList = () => {
     )
 }
 
-export default StepList;
+const mapStateToProps = state => {
+    return {
+        inProgress: state.stepReducer.inProgress,
+        serverError: state.stepReducer.serverError,
+        isServerError: state.stepReducer.isServerError,
+        stepsByTask: state.stepReducer.stepsByTask
+    }
+}
+
+export default connect(mapStateToProps, {})(StepList);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux"
 import { signUpReducer } from "./signUpReducer"
 import { requirementReducer } from "./requirementReducer"
+import { stepReducer } from "./stepsReducer"
 
 export default combineReducers({
   signUpReducer,
-  requirementReducer
+  requirementReducer,
+  stepReducer
 })
