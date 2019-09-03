c125110fcf551172e2784bbab0454057a00302a3
Adam Mathieson <mathieson1222@gmail.com>
2019-09-03 14:38:14 -0700

    format

--- src/components/steps/Step.js
+++ src/components/steps/Step.js
@@ -1,17 +1,17 @@
 import React from "react"
 import { CheckMark } from "./svg/CheckMark"
 
-
 const Step = ({ step, toggle }) => {
-//   console.log("On page render: ", step.is_complete)
+  //   console.log("On page render: ", step.is_complete)
 
   return (
     <div className="each-step">
-      <div className="check-box"
-      onClick={() => toggle(step.tasks_id, step.id, step.is_complete)}
+      <div
+        className="check-box"
+        onClick={() => toggle(step.tasks_id, step.id, step.is_complete)}
       >
         <div className={step.is_complete ? "complete" : "incomplete"}>
-            <CheckMark />
+          <CheckMark />
         </div>
       </div>
       <div className="description">{step.steps_description}</div>
--- src/components/steps/svg/CheckMark.js
+++ src/components/steps/svg/CheckMark.js
@@ -10,7 +10,7 @@ export const CheckMark = () => {
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        id='check'
+        id="check"
         fillRule="evenodd"
         clipRule="evenodd"
         d="M294.355 760.701L78.878 494.729L119.098 461.683L290.702 703.685L627.621 70.2734L668.878 108.229L294.355 760.701Z"
