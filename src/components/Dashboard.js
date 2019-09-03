460c877b578b1a9ea1d55456e2917cfcf18f54fe
hsrsbd <hsraffety@gmail.com>
2019-09-03 10:51:37 -0700

    Reimplementing Navigation.

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,12 @@
 import React from "react"
 import UserInfo from "./UserInfo"
 import RequirementCardContainer from "./requirement/RequirementCardContainer"
+import Navigation from "./Navigation"
 
 const Dashboard = () => {
   return (
     <div className="dash-container">
+      <Navigation />
       <UserInfo />
       <RequirementCardContainer />
     </div>
---
+++ src/components/Navigation/index.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react"
+import axios from "axios"
+import Logo from "../lib/Logo"
+
+import "./index.scss"
+
+const Navigation = () => {
+  const [user, setUser] = useState({ first_name: "" })
+  useEffect(() => {
+    const userId = localStorage.getItem("userId")
+    axios.get("http://localhost:5000/api/v0/users").then(res => {
+      const filtUser = res.data.filter(user => user.id === parseInt(userId))
+      setUser(filtUser[0])
+    })
+  }, [])
+  return (
+    <nav className="nav_wrapper">
+      <Logo />
+      <div className="user_info">
+        <div className="user">
+          <p className="user_full_name">
+            {user.first_name} {user.last_name}
+          </p>
+          <p className="user_track">Full Stack Web</p>
+        </div>
+        <div className="user_button">
+          <span></span>
+        </div>
+      </div>
+    </nav>
+  )
+}
+export default Navigation
