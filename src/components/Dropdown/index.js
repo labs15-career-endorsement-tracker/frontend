eb295de6422b110aa3b3c14b6cff1de129e62197
hsrsbd <hsraffety@gmail.com>
2019-09-03 18:23:25 -0700

    Updating functionality/styling.

--- src/components/Dropdown/index.js
+++ src/components/Dropdown/index.js
@@ -18,7 +18,11 @@ const Dropdown = () => {
         <i class="far fa-angle-down fa-3x"></i>
       </button>
       <div id="logoutPanel" className="dropdown-content">
-        <div className="logout" onClick={triggerLogout}>
+        <div
+          className="logout"
+          onClick={triggerLogout}
+          onMouseOut={triggerDropdown}
+        >
           <i className="fad fa-sign-out"></i>
           <p className="logout-text">Logout</p>
         </div>
