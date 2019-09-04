3986842384e726d7c1f9452d4b347007e0e15ac6
lschimm <lschimmelfennig@gmail.com>
2019-09-04 17:21:40 -0500

    Building out resourceAction

--- src/actions/resourceAction.js
+++ src/actions/resourceAction.js
@@ -1,1 +1,27 @@
 import { getRequirements } from "../api"
+import { loadAuthDataFromLocalStorage } from './../store';
+
+export const RESOURCE_FETCH_IN_PROGRESS = "RESOURCE_FETCH_IN_PROGRESS"
+export const RESOURCE_FETCH_SUCCESS = "RESOURCE_FETCH_SUCCESS"
+export const RESOURCE_FETCH_FAILURE = "RESOURCE_FETCH_FAILURE"
+
+const { token, userId } = loadAuthDataFromLocalStorage()
+
+export const getResourcesByRequirementId = reqId => dispatch => {
+    dispatch({
+        type: RESOURCE_FETCH_IN_PROGRESS
+    })
+    return getRequirements(token, reqId)
+        .then(resources => {
+            dispatch({
+                type: RESOURCE_FETCH_SUCCESS,
+                payload: resources
+            })
+        })
+        .catch(err =>
+            dispatch({
+                type: RESOURCE_FETCH_FAILURE,
+                payload: err.response.data
+            }))
+}
+
