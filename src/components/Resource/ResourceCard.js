688f4baf408bf4867ffd8bc7484ad21fd5e33c69
lschimm <lschimmelfennig@gmail.com>
2019-09-05 15:24:21 -0500

    Adds styles to resource components

--- src/components/Resource/ResourceCard.js
+++ src/components/Resource/ResourceCard.js
@@ -8,8 +8,14 @@ const ResourceCard = props => {
     <div className="resource-card">
       {props.resources.map(resource => (
         <div className="individual-resource" key={resource.id}>
-          <p className="resource-url"><a href={resource.url}>{resource.title}</a></p>
-          <p className="resource-type">{resource.type}</p>
+          <div className="individual-resource-icon">
+            <i class="fab fa-google-drive fa-2x"></i>
+          </div>
+          <div className="individual-resource-content">
+            <p className="resource-url">
+              <a href={resource.url}>{resource.title}</a>
+            </p>
+          </div>
         </div>
       ))}
     </div>
