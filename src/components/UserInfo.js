00bda3bdf671a6cb82adff125d5a5f3f8144ea26
Adam Mathieson <mathieson1222@gmail.com>
2019-09-05 13:41:26 -0700

    renders overall progress meter in userInfo component and adds borders for styling; removes unused code from stepList.scss

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -3,12 +3,13 @@ import React, { useState, useEffect } from "react"
 import { getUserById } from "../api"
 import { loadAuthDataFromLocalStorage } from "../store"
 
+import Percentage from './lib/Percentage'
+
 const UserInfo = () => {
   const [user, setUser] = useState({ first_name: "" })
 
   useEffect(() => {
     const { token, userId } = loadAuthDataFromLocalStorage()
-
     getUserById(token, userId)
       .then(user => setUser(user))
       .catch(err => console.log(err.response))
@@ -24,6 +25,9 @@ const UserInfo = () => {
           Welcome back, {user.first_name}.
           <br /> LET'S GET ENDRSD!
         </h2>
+      </div>
+      <div className='progress-meter'>
+        <Percentage progress={user.progress} />
       </div>
     </div>
   )
