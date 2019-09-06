abc348140c74fae8c16f1f7cb73ad31dc37ac89e
clem9281 <clem9281@gmail.com>
2019-09-05 17:55:58 -0700

    removes view steps text

--- src/components/requirement/ReqCard.js
+++ src/components/requirement/ReqCard.js
@@ -5,20 +5,13 @@ import StepGauge from "../../components/steps/StepGauge"
 
 const ReqCard = ({ requirement, fetchSteps, steps }) => {
   return (
-    <div className="requirement-card">
+    <div className="requirement-card" onClick={() =>
+      fetchSteps(loadFromLocalStorage("auth").token, requirement.id)
+    }>
       <StepGauge steps={steps} requirement={requirement} />
       <div className={`title-area ${requirement.progress === 100 ? 'complete' : 'incomplete'}`}>
       <h2 className={`title ${requirement.progress === 100 ? 'white-text' : ''}`}>{requirement.progress === 100 && <i className="fas fa-check"></i> }{requirement.title}</h2>
       </div>
-      {/* I was thinking this link could come out all together and the whole card should be a link to the next route */}
-      {/* <div
-        className="view-steps"
-        onClick={() =>
-          fetchSteps(loadFromLocalStorage("auth").token, requirement.id)
-        }
-      >
-        View Steps
-      </div> */}
     </div>
   )
 }
