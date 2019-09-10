b9155220b9f6520a14acd0838f0097b9dfe9fa4a
clem9281 <clem9281@gmail.com>
2019-09-10 13:43:35 -0700

    adds a full page loading component

---
+++ src/components/lib/Loaders/fullPageLoader.js
@@ -0,0 +1,21 @@
+import React from "react"
+import "./index.scss"
+import Loader from 'react-loader-spinner'
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
+
+const FullPageLoader = () => {
+  return (
+      <div  className="fullpage-loader">
+        <Loader
+        type="Watch"
+        color="#29AD44"
+        height={100}
+        width={100}
+        timeout={3000} //3 secs
+
+     />
+      </div>
+  )
+}
+
+export default FullPageLoader
