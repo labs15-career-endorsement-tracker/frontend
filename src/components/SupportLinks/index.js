94b1fb63823c7eef0a33bd68347c18a9a28ef3b5
lschimm <lschimmelfennig@gmail.com>
2019-09-17 18:54:17 -0500

    Fixes minor typos

--- src/components/SupportLinks/index.js
+++ src/components/SupportLinks/index.js
@@ -5,16 +5,16 @@ import "./index.scss"
 const SupportLinks = () => {
   return (
     <div className="support-link-icons">
-      <a className="twitter" href="https://twitter.com/getendrsd">
+      <a class="twitter" href="https://twitter.com/getendrsd">
         <i class="fab fa-twitter"></i>
       </a>
       <a
-        className="slack"
+        class="slack"
         href="https://lambdaschoolstudents.slack.com/messages/CN99JD9J5"
       >
         <i class="fab fa-slack"></i>
       </a>
-      <a className="email" href="">
+      <a class="email" href="">
         <i class="far fa-envelope"></i>
       </a>
     </div>
