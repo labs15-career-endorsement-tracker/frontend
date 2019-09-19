5b8f9ae76441467e84992ce00ea7dd515682ca3b
hsrsbd <hsraffety@gmail.com>
2019-09-19 13:45:30 -0700

    Minor styling alterations

--- src/components/SideBarRequirements/index.js
+++ src/components/SideBarRequirements/index.js
@@ -5,15 +5,19 @@ import "./index.scss"
 import { Form, SelectFormField } from "../lib"
 import { handleSubmit, formatRequirements } from "./utils"
 
-const SideBarRequirements = ({ handleSubmit, requirements }) => {
+const SideBarRequirements = ({ requirements }) => {
+  const styles = {
+    background: "red"
+  }
+
   return (
     <div className="sidebar-requirements-container">
-      <h1>Requirements</h1>
       <Form>
         <SelectFormField
           name="requirements"
-          labelText="Select a requirement"
+          labelText=""
           options={formatRequirements(requirements)}
+          styles={styles}
         ></SelectFormField>
       </Form>
     </div>
