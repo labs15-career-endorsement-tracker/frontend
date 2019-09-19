f0d0ab3c6fa5effe60ef3613be63ce5dc7b698da
Miikis <28997062+Miikis@users.noreply.github.com>
2019-09-19 06:33:00 -0700

    Add temp logout button until "My Account" page
    Signed-off-by: Miikis <28997062+Miikis@users.noreply.github.com>

--- src/components/lib/DashboardHeader/index.js
+++ src/components/lib/DashboardHeader/index.js
@@ -2,12 +2,19 @@ import React from "react"
 
 import "./index.scss"
 
+import { history } from "../../../store"
 import { Logo } from "../../lib"
+
+// TODO: Temporary; delete as soon as unnecessary
+const logout = () => {
+  localStorage.removeItem("auth")
+  history.push("/auth/sign-in")
+}
 
 const Notification = () => {
   return (
     <>
-      <i className="fas fa-bell"></i>
+      <i className="fas fa-bell" onClick={logout}></i>
     </>
   )
 }
