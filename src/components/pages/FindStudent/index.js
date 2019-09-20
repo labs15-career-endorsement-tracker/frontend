2672cead1dce91addac5616bd25a8d95e880c4f5
clem9281 <clem9281@gmail.com>
2019-09-20 14:18:53 -0700

    adds typing timer functionality

--- src/components/pages/FindStudent/index.js
+++ src/components/pages/FindStudent/index.js
@@ -1,9 +1,21 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
 
 import "./index.scss"
+import { handleSubmit } from "../PasswordResetForm/utils";
 
 const FindStudent = () => {
-    
+    const [search, setSearch] = useState('')
+    const [typingTimeout, setTypingTimeout] = useState(0);
+    const handleChanges = e => {
+        setSearch(e.target.value)
+        if (typingTimeout) {
+            clearTimeout(typingTimeout)
+        }
+        setTypingTimeout(setTimeout(() => handleSubmit(typingTimeout), 300))
+    }
+    const handleSubmit = () => {
+        console.log(typingTimeout)
+    }
     return (
         <section className="find-student-page">
             <header>
@@ -11,7 +23,7 @@ const FindStudent = () => {
                 <p>Search for a student by name</p>
             </header>
             <form>
-                <input className="search" type="text" aria-label="Search" placeholder="eg. Bob"/>
+                <input className="search" type="text" aria-label="Search" placeholder="eg. Bob" value={search}onChange={handleChanges}/>
                 <button className="search-btn"><i className="fas fa-search"></i></button>
             </form>
             <div>
