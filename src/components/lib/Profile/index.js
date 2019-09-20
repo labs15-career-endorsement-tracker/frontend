797fa28c4050800b454442a23bd9c85f6e7fcc62
hsrsbd <hsraffety@gmail.com>
2019-09-20 13:23:17 -0700

    Refines styles a tiny bit

--- src/components/lib/Profile/index.js
+++ src/components/lib/Profile/index.js
@@ -1,11 +1,14 @@
 import React from "react"
 import { NavLink } from "react-router-dom"
 
+import "./index.scss"
+
 const Profile = () => {
   return (
-    <div className="container">
+    <div className="profile-container">
       <NavLink to="/myaccount" className="my-account-link">
-        <i class="fas fa-user-edit"></i> My Account
+        <i className="fas fa-user-edit"></i>
+        <span className="account-text">My Account</span>
       </NavLink>
     </div>
   )
