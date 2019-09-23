01d35e16036ed0bd9031d429f23bd336c8bed47f
hsrsbd <hsraffety@gmail.com>
2019-09-23 14:49:51 -0700

    Working out student profile page.

--- src/components/pages/StudentProfile/index.js
+++ src/components/pages/StudentProfile/index.js
@@ -1,14 +1,17 @@
-import React from "react"
+import React, { useState } from "react"
 import { history } from "../../../store"
 
 const StudentProfile = () => {
   const location = history.location.pathname
   const { ...values } = location.split("/")
-  const student = Number(values[2])
-  console.log(student)
+  const studentId = Number(values[2])
+
+  const [student, setStudent] = useState()
+
+  console.log(studentId, student)
   return (
     <div>
-      <h1>{student}</h1>
+      <h1>{studentId}</h1>
     </div>
   )
 }
