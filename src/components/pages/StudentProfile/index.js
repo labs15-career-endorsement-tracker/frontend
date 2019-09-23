caef1d1d274eb917ea96586fe5f2432e8e13d9f4
hsrsbd <hsraffety@gmail.com>
2019-09-23 15:28:04 -0700

    Debugging

--- src/components/pages/StudentProfile/index.js
+++ src/components/pages/StudentProfile/index.js
@@ -1,17 +1,33 @@
-import React, { useState } from "react"
-import { history } from "../../../store"
+import React, { useState, useEffect } from "react"
+import { useSelector, useDispatch } from "react-redux"
+
+import { history, loadAuthDataFromLocalStorage } from "../../../store"
+import { fetchUser, fetchRequirements } from "../../../actions"
 
 const StudentProfile = () => {
+  const user = useSelector(state => state.userReducer.user)
   const location = history.location.pathname
   const { ...values } = location.split("/")
   const studentId = Number(values[2])
-
   const [student, setStudent] = useState()
 
-  console.log(studentId, student)
+  useEffect(() => {
+    const { token } = loadAuthDataFromLocalStorage()
+    setStudent(fetchUser(token, studentId))
+  }, [studentId])
+
+  console.log(
+    `This guy is from the useEffect, he is my student thing I made with a hook?:`,
+    student
+  )
   return (
     <div>
-      <h1>{studentId}</h1>
+      <h1>
+        <span>This guy right here is the ID of the student from the URL: </span>
+        {studentId}
+      </h1>
+      <span>This guy is the user from state: </span>
+      {user.first_name}
     </div>
   )
 }
