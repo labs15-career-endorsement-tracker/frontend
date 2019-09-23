9777a6de4b30a41d2090e3d7c3e019d84dd26ac2
Miikis <28997062+Miikis@users.noreply.github.com>
2019-09-23 00:47:59 -0700

    Style dashboard header to accomodate logo
    Signed-off-by: Miikis <28997062+Miikis@users.noreply.github.com>

--- src/components/lib/DashboardHeader/index.js
+++ src/components/lib/DashboardHeader/index.js
@@ -1,31 +1,13 @@
 import React from "react"
 
-import "./index.scss"
-
-import { history } from "../../../store"
 import { Logo } from "../../lib"
 
-// TODO: Temporary; delete as soon as unnecessary
-const logout = () => {
-  localStorage.removeItem("auth")
-  history.push("/auth/sign-in")
-}
-
-const Notification = () => {
-  return (
-    <>
-      <i className="fas fa-bell" onClick={logout}></i>
-    </>
-  )
-}
+import "./index.scss"
 
 const DashboardHeader = () => {
   return (
     <header className="dashboard-header">
       <Logo></Logo>
-      <nav>
-        <Notification></Notification>
-      </nav>
     </header>
   )
 }
