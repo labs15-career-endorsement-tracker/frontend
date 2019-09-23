a24e8da55c695d9d98a99b5c5709e3f85f33b857
Miikis <28997062+Miikis@users.noreply.github.com>
2019-09-22 23:09:50 -0700

    Setup SidebarHeader component
    Signed-off-by: Miikis <28997062+Miikis@users.noreply.github.com>

---
+++ src/components/lib/SidebarHeader/index.js
@@ -0,0 +1,22 @@
+import React from "react"
+import { useSelector } from "react-redux"
+
+import { useGreeting } from "../../../hooks"
+import { ProgressRing } from "../../lib"
+
+import "./index.scss"
+
+const SidebarHeader = () => {
+  const { first_name, progress } = useSelector(state => state.userReducer.user)
+  const greeting = useGreeting()
+
+  return (
+    <header className="sidebar__header">
+      <ProgressRing progressValue={progress}></ProgressRing>
+      <h3 className="greeting">{greeting + ","}</h3>
+      <h2 className="greeting-name">{first_name}</h2>
+    </header>
+  )
+}
+
+export default SidebarHeader
