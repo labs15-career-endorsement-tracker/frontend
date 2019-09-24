6889c5cd57d023c324908abc46dbd3ba35072cc8
hsrsbd <hsraffety@gmail.com>
2019-09-24 11:37:51 -0700

    confusion resolution

--- src/components/lib/ProfileCard/index.js
+++ src/components/lib/ProfileCard/index.js
@@ -5,36 +5,31 @@ import uuid from "uuid/v4"
 import "./index.scss"
 
 const ProfileCard = ({ student, requirements, showFull }) => {
-  console.log(`***********`, student, requirements)
-
-  console.log(showFull)
   return (
-    <div className="big-container">
+    <>
       <p className="profile-track">Full-Stack Web Development</p>
       <p className="profile-first">{student.first_name}</p>
       <p className="profile-last">{student.last_name}</p>
-      <div className="test">
-        {showFull === "false" ? (
-          <div className="overall-progress" key={uuid()}>
+      {showFull === "false" ? (
+        <div className="overall-progress" key={uuid()}>
+          <p>Overall Progress:</p>
+          <ProgressRing progressValue={student.progress}></ProgressRing>
+        </div>
+      ) : (
+        <div className="req-container" key={`$student.id`}>
+          <div className="overall-progress">
             <p>Overall Progress:</p>
             <ProgressRing progressValue={student.progress}></ProgressRing>
           </div>
-        ) : (
-          <div className="req-container" key={`$student.id`}>
-            <div className="overall-progress">
-              <p>Overall Progress:</p>
-              <ProgressRing progressValue={student.progress}></ProgressRing>
+          {requirements.map(req => (
+            <div className="progress-card" key={uuid()}>
+              <p className="req-title">{req.title}</p>
+              <ProgressRing progressValue={req.progress}></ProgressRing>
             </div>
-            {requirements.map(req => (
-              <div className="progress-card" key={uuid()}>
-                <p className="req-title">{req.title}</p>
-                <ProgressRing progressValue={req.progress}></ProgressRing>
-              </div>
-            ))}
-          </div>
-        )}
-      </div>
-    </div>
+          ))}
+        </div>
+      )}
+    </>
   )
 }
 
