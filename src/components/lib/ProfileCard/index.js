36f01c795671e574ef59c64fa087a2766dcb2c7b
hsrsbd <hsraffety@gmail.com>
2019-09-23 21:06:29 -0700

    Many updates.

--- src/components/lib/ProfileCard/index.js
+++ src/components/lib/ProfileCard/index.js
@@ -1,11 +1,33 @@
 import React from "react"
+import { ProgressRing, RequirementCard } from "../../lib"
 
-const ProfileCard = ({ student }) => {
+import "./index.scss"
+
+const ProfileCard = ({ student, requirements }) => {
+  console.log(student, requirements)
+
   return (
-    <div>
+    <div className="big-container">
       <p>
         {student.first_name} {student.last_name}
       </p>
+      <p>{student.email}</p>
+      <div class="test">
+        <div className="overall-progress">
+          <p>Overall Progress:</p>
+          <ProgressRing progressValue={student.progress}></ProgressRing>
+        </div>
+        <div className="req-container">
+          {requirements.map(req => (
+            <>
+              <div className="progress-card">
+                <p className="req-title">{req.title}</p>
+                <ProgressRing progressValue={req.progress}></ProgressRing>
+              </div>
+            </>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
