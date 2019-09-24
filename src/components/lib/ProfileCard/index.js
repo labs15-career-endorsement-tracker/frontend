7e26bf4ae79889aa53a81fa4aaac9c775e240a7c
hsrsbd <hsraffety@gmail.com>
2019-09-24 11:20:37 -0700

    Merge conflict resolution.

--- src/components/lib/ProfileCard/index.js
+++ src/components/lib/ProfileCard/index.js
@@ -10,21 +10,6 @@ const ProfileCard = ({ student, requirements, showFull }) => {
   console.log(showFull)
   return (
     <div className="big-container">
-<<<<<<< HEAD
-      <div className="profile-info">
-        <p className="profile-track">Full-Stack Web Development</p>
-        <p className="profile-first">{student.first_name}</p>{" "}
-        <p className="profile-last">{student.last_name}</p>
-        {/* <p>{student.email}</p> */}
-      </div>
-      <div className="profile-actions">
-        <p>
-          <i class="fas fa-user-minus"></i> Unassign
-        </p>
-        <div className="test">
-          {showFull === "false" ? (
-            <div className="overall-progress" key={uuid()}>
-=======
       <p className="profile-track">Full-Stack Web Development</p>
       <p className="profile-first">{student.first_name}</p>
       <p className="profile-last">{student.last_name}</p>
@@ -37,25 +22,17 @@ const ProfileCard = ({ student, requirements, showFull }) => {
         ) : (
           <div className="req-container" key={`$student.id`}>
             <div className="overall-progress">
->>>>>>> 23b0e01d6878ae3384a6fea5cabeb4f5dadad6c1
               <p>Overall Progress:</p>
               <ProgressRing progressValue={student.progress}></ProgressRing>
             </div>
-          ) : (
-            <div className="req-container" key={`$student.id`}>
-              <div className="overall-progress">
-                <p>Overall Progress:</p>
-                <ProgressRing progressValue={student.progress}></ProgressRing>
+            {requirements.map(req => (
+              <div className="progress-card" key={uuid()}>
+                <p className="req-title">{req.title}</p>
+                <ProgressRing progressValue={req.progress}></ProgressRing>
               </div>
-              {requirements.map(req => (
-                <div className="progress-card" key={uuid()}>
-                  <p className="req-title">{req.title}</p>
-                  <ProgressRing progressValue={req.progress}></ProgressRing>
-                </div>
-              ))}
-            </div>
-          )}
-        </div>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   )
