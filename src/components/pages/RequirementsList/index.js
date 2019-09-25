bbac0a859c949381f534744dbce81153f845059b
Miikis <28997062+Miikis@users.noreply.github.com>
2019-09-25 01:43:12 -0700

    Wrap RequirementsList in layout components
    Signed-off-by: Miikis <28997062+Miikis@users.noreply.github.com>

--- src/components/pages/RequirementsList/index.js
+++ src/components/pages/RequirementsList/index.js
@@ -4,25 +4,31 @@ import { useSelector } from "react-redux"
 import "./index.scss"
 
 import { RequirementCard } from "../../lib"
+import { ContentHeader, DashboardContent } from "../../layout"
 
 const RequirementsList = () => {
   const requirements = useSelector(
     state => state.requirementReducer.requirements
   )
-  const { tracks_title } = useSelector(state => state.userReducer.user)
+  const { tracks_title, progress } = useSelector(
+    state => state.userReducer.user
+  )
 
   return (
-    <section className="requirement-card-container">
-      <hgroup>
-        <h2>Requirements</h2>
-        <h3>{tracks_title}</h3>
-      </hgroup>
-      {requirements.map(req => (
-        <div key={req.id} className="req-card-area">
-          <RequirementCard requirement={req} />
-        </div>
-      ))}
-    </section>
+    <DashboardContent>
+      <ContentHeader
+        title="Requirements"
+        prompt={tracks_title}
+        progress={progress}
+      ></ContentHeader>
+      <section className="requirement-card-container">
+        {requirements.map(req => (
+          <div key={req.id} className="req-card-area">
+            <RequirementCard requirement={req} />
+          </div>
+        ))}
+      </section>
+    </DashboardContent>
   )
 }
 
