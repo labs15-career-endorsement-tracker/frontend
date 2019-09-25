53102e7f8ae9cabc917de105e47c4ab267135c57
clem9281 <clem9281@gmail.com>
2019-09-24 17:08:08 -0700

    adds styles to card, with circle and line. Removes the redirect from requirement step list: where it checks if requirement and if not redirect. This was causing a redirect if you refreshed the page on requirements/:id, which in turn caused a memory leak

--- src/components/lib/RequirementStepList/index.js
+++ src/components/lib/RequirementStepList/index.js
@@ -1,13 +1,11 @@
-import React, { useState, useEffect } from "react"
+import React from "react"
 
 import { withRouter, Redirect } from "react-router"
-import { NavLink } from "react-router-dom"
 
 import "./index.scss"
 
-import { RequirementStep, ProgressRing } from "../../lib"
+import { RequirementStep } from "../../lib"
 
-// import { AssignmentStep } from "../../lib"
 
 const StepList = ({
   requirement,
@@ -15,7 +13,7 @@ const StepList = ({
   toggleStep,
   fetchUser
 }) => {
-  if (!requirement) return <Redirect to="/requirements"></Redirect>
+  // if (!requirement) return <Redirect to="/requirements"></Redirect>
   return (
     <div className="step-list-container">
       <div className="step-list">
@@ -29,14 +27,6 @@ const StepList = ({
         ))}
         {/* We left this here in case you want it: it's part of an old design */}
         {/* <AssignmentStep /> */}
-        <div className="return-requirements-container">
-          <div className="return-box">
-            <NavLink to="/" className="back-link">
-              <i className="fad fa-long-arrow-left"></i>
-              <span className="return-text">Return to requirements</span>
-            </NavLink>
-          </div>
-        </div>
       </div>
     </div>
   )
