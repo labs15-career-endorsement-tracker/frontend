b889299364bac39bf9b4dc0ca879deade20fb369
Miikis <28997062+Miikis@users.noreply.github.com>
2019-09-25 01:41:03 -0700

    Add a DashboardContent Component
    Signed-off-by: Miikis <28997062+Miikis@users.noreply.github.com>

---
+++ src/components/layout/DashboardContent/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+import "./index.scss"
+
+const DashboardContent = ({ children }) => {
+  return <main className="content">{children}</main>
+}
+
+export default DashboardContent
