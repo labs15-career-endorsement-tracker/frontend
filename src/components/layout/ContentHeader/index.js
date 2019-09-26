73dab8a85823de45fedea48e52b10779ea47ba85
clem9281 <clem9281@gmail.com>
2019-09-26 16:13:33 -0700

    merge develop

--- src/components/layout/ContentHeader/index.js
+++ src/components/layout/ContentHeader/index.js
@@ -7,13 +7,9 @@ import {useMenu} from "../../../hooks"
 import {MenuBurger} from "../../lib"
 import "./index.scss"
 
-<<<<<<< HEAD
 
 const DashboardHeader = ({ title, prompt, progress }) => {
   const {isOpen} = useMenu()
-=======
-const DashboardHeader = ({ title, prompt, progress = 0 }) => {
->>>>>>> 3eb9357a95718802c969b5ac7e4f2b09586b7bfd
   return (
     <header className="header">
       <MenuBurger isOpen={isOpen} />
