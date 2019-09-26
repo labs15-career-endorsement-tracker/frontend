2baa2b611230fa9bc65282c5701bf408ae50907e
hsrsbd <hsraffety@gmail.com>
2019-09-26 10:44:20 -0700

    Initial commit for styled individual profile cards.

--- src/components/lib/PinnedStudentCard/index.js
+++ src/components/lib/PinnedStudentCard/index.js
@@ -1,13 +1,32 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import { ContentHeader, DashboardContent } from "../../layout"
 
+import { ProgressRing } from "../../lib"
+import uuid from "uuid/v4"
+import { loadAuthDataFromLocalStorage } from "../../../store"
+import { togglePinnedStudent } from "../../../actions"
+import { useDispatch } from "react-redux"
 import "./index.scss"
 
 const PinnedStudentCard = ({ student }) => {
-  const { first_name: firstName } = student
+  const dispatch = useDispatch()
+  const { token } = loadAuthDataFromLocalStorage()
+
+  const handleToggle = e => {
+    e.preventDefault()
+    dispatch(togglePinnedStudent(token, student.id))
+  }
+
   return (
-    <div className="pinned-student">
-      <Link to={`/student/${student.id}`}>{firstName}</Link>
+    <div className="pinned-student-card" key={uuid()}>
+      <div className="inner-pinned-student-card">
+        {student.first_name} {student.last_name}
+        <p className="toggle-assign">
+          <i onClick={handleToggle} className={`fad fa-thumbtack`}></i>
+        </p>
+        <ProgressRing progressValue={student.progress}></ProgressRing>
+      </div>
     </div>
   )
 }
--- src/components/pages/MyStudents/index.js
+++ src/components/pages/MyStudents/index.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react"
 import { useSelector, useDispatch } from "react-redux"
 import { fetchPinnedStudents } from "../../../actions"
 import { Link } from "react-router-dom"
-import ProfileCard from "../../lib/ProfileCard"
+import PinnedStudentCard from "../../lib/PinnedStudentCard"
 import FindStudent from "../../pages/FindStudent"
 
 import { ContentHeader, DashboardContent } from "../../layout"
@@ -22,25 +22,21 @@ const MyStudents = () => {
     dispatch(fetchPinnedStudents())
   }, [dispatch])
   return (
-    <>
-      <DashboardContent>
-        <ContentHeader
-          title={"Find a Student"}
-          prompt={"Search for a student by name"}
-          progress={-1}
-        ></ContentHeader>
-        <FindStudent />
-        <section className="my-pinned-students">
-          {pinnedStudents.map(student => (
-            <div className="student-profile-card" key={student.id}>
-              <Link to={`/student/${student.id}`}>
-                <ProfileCard student={student} showFull={"false"} />
-              </Link>
-            </div>
-          ))}
-        </section>
-      </DashboardContent>
-    </>
+    <DashboardContent>
+      <ContentHeader
+        title={"Find a Student"}
+        prompt={"Search for a student by name"}
+        progress={-1}
+      ></ContentHeader>
+      <FindStudent />
+      {pinnedStudents.map(student => (
+        <div className="student-profile-card" key={student.id}>
+          <Link to={`/student/${student.id}`}>
+            <PinnedStudentCard student={student}></PinnedStudentCard>
+          </Link>
+        </div>
+      ))}
+    </DashboardContent>
   )
 }
 
