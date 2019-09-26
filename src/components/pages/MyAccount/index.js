a868f02c4838991f1f66e26d0a392d7d5eac58d8
clem9281 <clem9281@gmail.com>
2019-09-25 17:32:46 -0700

    adds progress styles, adjusts ProgressRing to take more props

--- src/components/pages/MyAccount/index.js
+++ src/components/pages/MyAccount/index.js
@@ -11,6 +11,8 @@ import { updateUserCalendly } from "../../../api"
 
 import { DashboardContent, ContentHeader } from "../../layout"
 
+import { ProgressRing }from '../../lib'
+
 const MyAccount = () => {
   const dispatch = useDispatch()
   const user = useSelector(state => state.userReducer.user)
@@ -59,54 +61,58 @@ const MyAccount = () => {
         </div>
 
         <div className="account-info">
-        <div className="coach-calendly">
-        {user.is_admin ? (
-            <form onSubmit={handleSubmit}>
-              <label htmlFor="coach-calendly-link">Update Your Calendly Link</label>
-              {/* <div> */}
-              <input
-              id="coach-calendly-link"
-                name="calendly_link"
-                placeholder={user.calendly_link || "Enter Your Calendly Link"}
-                value={userData.calendly_link}
-                onChange={handleChange}
-              />
-              <button>Upload</button>
-              {/* </div> */}
-            </form>
-          ) :
-            user.coach ?
-            (
-              <>
-              <p>Your coach is <span className="coach-name">Coach Name</span></p>
-              <button>Schedule a meeting</button>
-              </>
-            )
-            :
-            <p>
-              When you have been assigned to a coach, you'll be able to schedule a meeting here
+          <div className="coach-calendly">
+            {/* if the user is an admin display the update calendly form */}
+            {user.is_admin ? (
+              <form onSubmit={handleSubmit}>
+                <label htmlFor="coach-calendly-link">Update Your Calendly Link</label>
+                {/* <div> */}
+                <input
+                  id="coach-calendly-link"
+                  name="calendly_link"
+                  placeholder={user.calendly_link || "Enter Your Calendly Link"}
+                  value={userData.calendly_link}
+                  onChange={handleChange}
+                />
+                <button>Upload</button>
+                {/* </div> */}
+              </form>
+            ) :
+            // If the user has a coach display the schedule a meeting button 
+              user.coach ?
+                (
+                  <>
+                    <p>Your coach is <span className="coach-name">Coach Name</span></p>
+                    <button>Schedule a meeting</button>
+                  </>
+                )
+                // else display the you'll be able to schedule a meeting soon message
+                :
+                <p>
+                  When you have been assigned to a coach, you'll be able to schedule a meeting here
             </p>
-          }
+            }
           </div>
           <div className="password-logout">
             <div className="reset-password">
-            <i className="fas fa-key"></i>
-            <NavLink to="/auth/reset-password">Change Password</NavLink>
-            </div>
-            <div className="logout">
-            <i class="fas fa-sign-out-alt"></i>
-            <button
-            onClick={() => {
-              clearAuthDataFromLocalStorage()
-              dispatch(logout())
-              setTimeout(() => history.push("/"), 300)
-            }}
-          >
-            Logout
-          </button> 
+              <i className="fas fa-key"></i>
+              <NavLink to="/auth/reset-password">Reset Password</NavLink>
             </div>
 
-          </div> 
+            {!user.is_admin && <ProgressRing startColor="#081534" endColor="#081534" progressValue={25} trailColor="#ffffff" strokeOpacity={1} strokeLinecap = "butt"></ProgressRing>}
+            <div className="logout">
+              <i className="fas fa-sign-out-alt"></i>
+              <button
+                onClick={() => {
+                  clearAuthDataFromLocalStorage()
+                  dispatch(logout())
+                  setTimeout(() => history.push("/"), 300)
+                }}
+              >
+                Logout
+            </button>
+            </div>
+          </div>
         </div>
       </div>
 
